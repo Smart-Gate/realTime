01f5423fc364726092af1b3c3001fa8c41caafdf
Yousef Alatassi <yousef.1.618a@gmail.com>
2019-12-31 00:38:09 +0100

    adding relations

--- app/User.php
+++ app/User.php
@@ -5,7 +5,9 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-
+use App\Model\Reply;
+use App\Model\Question;
+use App\Model\Like;
 class User extends Authenticatable
 {
     use Notifiable;
@@ -36,4 +38,16 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function questiosns()
+    {
+        return $this->hasMany(Question::class);
+    }
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
 }
